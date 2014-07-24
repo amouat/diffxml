144789cea32c4e5ac464c98764b6d058848011d7
Adrian Mouat <adrian.mouat@gmail.com>
2014-07-24 13:19:55 +0100

    Allow spaces in JAVA_HOME

--- diffxml.cmd
+++ diffxml.cmd
@@ -1,7 +1,7 @@
 @rem Utility script to call diffxml with the given arguments
 @echo off
 IF DEFINED JAVA_HOME (
-%JAVA_HOME%\bin\java.exe -cp "%~dp0\lib\diffxml.jar" org.diffxml.diffxml.DiffXML %*
+"%JAVA_HOME%\bin\java.exe" -cp "%~dp0\lib\diffxml.jar" org.diffxml.diffxml.DiffXML %*
 ) ELSE (
 java -cp "%~dp0\lib\diffxml.jar" org.diffxml.diffxml.DiffXML %*
 )
